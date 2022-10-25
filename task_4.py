9e538c0082de5496353d50ed8743973ed88097a8
Feck-it-im-in <115720738+Feck-it-im-in@users.noreply.github.com>
2022-10-25 20:37:02 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,5 +1,11 @@
 def snowball(n, k):
-    pass
+    r = n / 2
+    for i in range(n):
+        for j in range(n):
+            if ((i - r) ** 2 + (j - r) ** 2 <= r ** 2):
+                print("#", end="")
+            else:
+                print (" ", end="")
 
 
 def snowman(n):
